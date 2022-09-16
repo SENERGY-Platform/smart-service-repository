8edff15fdad84412620fd0525e9b1bb5318f165f
Ingo <roessner@infai.org>
2022-09-16 11:53:06 +0200

    sort options

--- pkg/controller/releases.go
+++ pkg/controller/releases.go
@@ -299,6 +299,11 @@ func (this *Controller) GetReleaseParameterWithoutAuthCheck(token auth.Token, id
 			}
 		}
 
+		//sort options
+		sort.Slice(param.Options, func(i, j int) bool {
+			return param.Options[i].Label < param.Options[j].Label
+		})
+
 		result = append(result, param)
 	}
 	sort.Slice(result, func(i, j int) bool {
