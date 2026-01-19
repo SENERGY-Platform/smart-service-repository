d9f4126fb3415c578823c9222fc8bdb74a831939
franzmueller <franzmueller@users.noreply.github.com.>
2026-01-19 08:48:40 +0100

    add test debug log

--- pkg/tests/docker/util.go
+++ pkg/tests/docker/util.go
@@ -1,10 +1,21 @@
 package docker
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"os"
 )
 
 func inCIEnv() bool {
-	os.Getenv("CI")
-	return os.Getenv("CI") == "true"
+	ci := os.Getenv("CI") == "true"
+	if !ci {
+		log.Println("Not in CI")
+		b, err := json.MarshalIndent(os.Environ(), "", "  ")
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+		log.Printf("Env:\n%s\n", string(b))
+	}
+	return ci
 }
